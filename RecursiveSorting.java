885452baa07b9486c0922c164d1a2a76e4c13b8a
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-02-20 09:56:57 +0530

    Add files via upload

---
+++ RecursiveSorting.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class RecursiveSorting {
+    public static void sort(int[] arr, int l){
+        if(l==1) return;
+        sort(arr,l-1);
+        int e = arr[l-2];
+        insert(arr, l-1, arr[l-1]);
+
+    }
+    public static void insert(int[] arr, int k, int n){
+        if(k==0){
+            arr[0]=n;
+            return;
+        }
+        if(arr[k-1]<n){
+            return;
+        }
+        swap(arr,k-1,n);
+        insert(arr,k-1,n);
+    }
+    public static void swap(int[] arr, int index, int n){
+        int c = arr[index];
+        arr[index] = n;
+        arr[index+1] = c;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,9,5,2,8,3,4};
+        sort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
