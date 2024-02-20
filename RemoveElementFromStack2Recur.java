d1c785faf7df41718de8594efb286d29309b6b10
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-02-20 15:56:28 +0530

    Add files via upload

---
+++ RemoveElementFromStack2Recur.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class RemoveElementFromStack2Recur {
+
+        public static void deleteMid(Stack<Integer> stack, int mid){
+            if(stack.size()==0) return;
+            if(mid==1) {
+                stack.pop();
+                return;
+            }
+            int e = stack.pop();
+            deleteMid(stack,mid-1);
+            stack.push(e);
+        }
+        public static void main(String[] args) {
+            Stack<Integer> stack = new Stack<>();
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            stack.push(4);
+            stack.push(5);
+            stack.push(6);
+            stack.push(7);
+            stack.push(8);
+            stack.push(9);
+            stack.push(10);
+            deleteMid(stack, (stack.size()/2) + 1);
+            System.out.println(stack);
+        }
+
+
+}
