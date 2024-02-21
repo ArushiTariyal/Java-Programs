e40cb5f6d8813421d17ec0479925d1d39d7333f9
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-02-21 19:20:42 +0530

    Add files via upload

---
+++ RecursionForStringSubsets.java
@@ -0,0 +1,19 @@
+public class RecursionForStringSubsets {
+    public static void solve(String ip, String op){
+        if(ip.length()==0){
+            System.out.println(op);
+            return;
+        }
+        String op1 = op;
+        String op2 = op;
+        op2 = op2 + ip.charAt(0);
+        ip = ip.substring(1);
+        solve(ip,op1);
+        solve(ip,op2);
+    }
+    public static void main(String[] args) {
+        String ip = "abcd";
+        String op = "";
+        solve(ip,op);
+    }
+}
