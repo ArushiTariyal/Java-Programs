54a6a1fbfca4b229a8d96c4d09c1f129d66c9cd4
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-03-07 15:00:48 +0530

    Add files via upload

---
+++ LongestSubArrayWithSumKForAllIntegersOptimal.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubArrayWithSumKForAllIntegersOptimal {
+    public static void longestSubArrayWithSumK(int []a, long k) {
+        Map<Long,Integer> m= new HashMap<>();
+        long sum = 0;
+        int len = 0;
+        for(int i = 0; i<=a.length-1; i++){
+            sum += a[i];
+            if(!m.containsKey(sum)) m.put(sum,i);
+            if(sum==k) len = i+1;
+            else if(sum>k){
+                if(m.containsKey(sum-k)){
+                    len = Math.max(len,i- m.get(sum-k));
+                }
+            }
+        }
+        System.out.println(len);;
+
+    }
+    public static void main(String[] args){
+        longestSubArrayWithSumK(new int[]{1,2,3,1,1,1,1},3);
+    }
+}
