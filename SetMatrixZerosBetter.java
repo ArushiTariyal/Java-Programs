cb3cc1f236e2f668adfb037818a0f121156b01b5
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-03-28 19:55:37 +0530

    Add files via upload

---
+++ SetMatrixZerosBetter.java
@@ -0,0 +1,21 @@
+public class SetMatrixZerosBetter {
+    class Solution {
+        public void setZeroes(int[][] matrix) {
+            int[] cols = new int[matrix[0].length];
+            int[] rows = new int[matrix.length];
+            for(int i = 0; i<matrix.length; i++){
+                for(int j = 0; j<matrix[0].length; j++){
+                    if(matrix[i][j]==0){
+                        cols[j] = 1;
+                        rows[i] = 1;
+                    }
+                }
+            }
+            for(int i = 0; i<matrix.length; i++){
+                for(int j = 0; j<matrix[0].length; j++){
+                    if(rows[i]==1 || cols[j]==1) matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
