11090eb704ac2d17ec2dffac442ab917301a80ac
ArushiTariyal <112552758+ArushiTariyal@users.noreply.github.com>
2024-03-31 11:07:42 +0530

    Add files via upload

---
+++ SpiralMatrix.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            int l = 0;
+            int r = matrix[0].length-1;
+            int t = 0;
+            int b = matrix.length-1;
+            List<Integer> ans = new ArrayList<>();
+            while(t<=b && l <= r){
+                for(int i = l; i<=r; i++){
+                    ans.add(matrix[t][i]);
+                }
+                t++;
+                for(int i = t; i<=b; i++){
+                    ans.add(matrix[i][r]);
+                }
+                r--;
+                if(t<=b){
+                    for(int i = r; i>=l; i--){
+                        ans.add(matrix[b][i]);
+                    }
+                }
+                b--;
+                if(l<=r){
+                    for(int i = b; i>=t; i--){
+                        ans.add(matrix[i][l]);
+                    }
+                    l++;
+                }
+            }
+            return ans;
+        }
+    }
+}
