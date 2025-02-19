1b673bde223c1298209effbeb5cd3ef879535390
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-02-19 16:13:12 +0530

    Add files via upload

---
+++ AllocateBooks_Optimal.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class AllocateBooks_Optimal {
+    public class Solution {
+        public static int findPages(ArrayList<Integer> arr, int n, int m) {
+            // Write your code here.
+            if(m>n) return -1;
+            int max = Integer.MIN_VALUE;
+            int sum = 0;
+            for(int i = 0; i<arr.size(); i++){
+                max = Math.max(max, arr.get(i));
+                sum += arr.get(i);
+            }
+            int low = max, high = sum;
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                if(studentCount(arr,m,n,mid)) high = mid-1;
+                else low = mid+1;
+            }
+            return low;
+        }
+        public static boolean studentCount(ArrayList<Integer> arr, int students, int books, int pages){
+            int stuCount= 1, allocated = 0;
+            for(int i = 0; i<arr.size(); i++){
+                if(arr.get(i)+allocated<=pages){
+                    allocated += arr.get(i);
+                }
+                else{
+                    stuCount++;
+                    allocated = arr.get(i);
+                }
+            }
+            if(stuCount<=students) return true;
+            return false;
+        }
+    }
+}
