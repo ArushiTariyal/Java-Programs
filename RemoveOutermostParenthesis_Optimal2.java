e6ab125fa3a019313d11f0c82e20babaabf52aa6
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-02-22 17:53:31 +0530

    Add files via upload

---
+++ RemoveOutermostParenthesis_Optimal2.java
@@ -0,0 +1,14 @@
+public class RemoveOutermostParenthesis_Optimal2 {
+    class Solution {
+        public String removeOuterParentheses(String s) {
+            int cnt = 0;
+            String ans = "";
+            for(int i = 0; i<s.length(); i++){
+                if(s.charAt(i)==')') cnt--;
+                if(cnt !=0) ans += s.charAt(i);
+                if(s.charAt(i)=='(') cnt++;
+            }
+            return ans;
+        }
+    }
+}
