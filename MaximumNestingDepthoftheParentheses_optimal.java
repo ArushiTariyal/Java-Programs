416e68fe23a054babca1456a3f0d953b3d799cd5
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-03-10 16:35:58 +0530

    Add files via upload

--- MaximumNestingDepthoftheParentheses_optimal.java
+++ MaximumNestingDepthoftheParentheses_optimal.java
@@ -5,11 +5,7 @@ public class MaximumNestingDepthoftheParentheses_optimal {
         int curr = 0;
         for(int i = 0; i<s.length(); i++){
             if(s.charAt(i)=='('){
-                curr++;
-            }
-            else if(s.charAt(i)==')'){
-                max = Math.max(curr,max);
-                curr--;
+
             }
         }
         return max;
