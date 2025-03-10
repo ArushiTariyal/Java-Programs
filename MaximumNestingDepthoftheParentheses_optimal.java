4c55b7c3ffb16a5089afe8f3e949b15b9cefe8e7
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-03-10 15:37:08 +0530

    Add files via upload

---
+++ MaximumNestingDepthoftheParentheses_optimal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MaximumNestingDepthoftheParentheses_optimal {
+    public static int maxDepth(String s) {
+        int max = 0;
+        int curr = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                curr++;
+            }
+            else if(s.charAt(i)==')'){
+                max = Math.max(curr,max);
+                curr--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(maxDepth(str));
+    }
+
+
+}
