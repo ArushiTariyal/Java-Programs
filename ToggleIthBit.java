ec75771002f1522133ec499ab6df71927a6e3bfa
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-06-01 13:51:19 +0530

    Add files via upload

---
+++ ToggleIthBit.java
@@ -0,0 +1,8 @@
+public class ToggleIthBit {
+    public static void main(String[] args){
+        int num = 13;
+        int i = 2;
+        int bit = 1;
+        System.out.println(num ^ (bit<<i));
+    }
+}
