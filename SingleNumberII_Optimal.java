150ad6dd8a4d1ed308aa0f766f9b6ef3c6e83aaa
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-06-02 12:02:37 +0530

    Add files via upload

---
+++ SingleNumberII_Optimal.java
@@ -0,0 +1,19 @@
+public class SingleNumberII_Optimal {
+    public static void main(String[] args){
+        int[] nums = {5,5,5,6,4,4,4};
+        int ans = 0;
+        for(int bits = 0; bits<=31; bits++){
+            int k = 1<<bits;
+            int count = 0;
+            for(int i = 0; i<nums.length; i++) {
+                if((nums[i] & k)!=0) {
+                    count++;
+                }
+            }
+            if(count%3==1){
+                ans = (ans | k);
+            }
+        }
+        System.out.println(ans);
+    }
+}
