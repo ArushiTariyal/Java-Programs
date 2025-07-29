c92335c00e6103737f40e0afa708ad3abdea4217
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2025-07-29 17:05:44 +0530

    Add files via upload

---
+++ LemonadeChange_Greedy_Re.java
@@ -0,0 +1,30 @@
+public class LemonadeChange_Greedy_Re {
+    class Solution {
+        public boolean lemonadeChange(int[] bills) {
+            int five = 0;
+            int ten = 0;
+
+            for(int i = 0; i<bills.length; i++){
+                if(bills[i]==5) five++;
+                else if(bills[i]==10){
+                    if(five>0){
+                        five--;
+                        ten++;
+                    }
+                    else return false;
+                }
+                else{
+                    if(five>=1 && ten>=1){
+                        five -= 1;
+                        ten--;
+                    }
+                    else if(five>=3){
+                        five -=3;
+                    }
+                    else return false;
+                }
+            }
+            return true;
+        }
+    }
+}
