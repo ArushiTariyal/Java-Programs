8ac2e4dc52d81e8be648596f49b24f2c817561b5
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2026-04-06 11:19:32 +0530

    Add files via upload

---
+++ FindMinInBT.java
@@ -0,0 +1,20 @@
+public class FindMinInBT {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left,TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Solution {
+        public TreeNode searchMinBST(TreeNode root) {
+            if(root.right!=null) return searchMinBST(root.right);
+            return root;
+        }
+    }
+}
