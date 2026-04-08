298e48169b40ce24ca5b027d18a6e2318d05297f
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2026-04-08 11:37:29 +0530

    Add files via upload

---
+++ ValidateBST.java
@@ -0,0 +1,29 @@
+public class ValidateBST {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        public boolean isValidBSTCheck(TreeNode root, TreeNode min, TreeNode max){
+            if(root==null) return true;
+
+            if(min!=null && root.val<=min.val) return false;
+            if(max!=null && root.val>=max.val) return false;
+
+            return isValidBSTCheck(root.left,min,root) && isValidBSTCheck(root.right,root,max);
+        }
+        public boolean isValidBST(TreeNode root) {
+            return isValidBSTCheck(root,null, null);
+        }
+    }
+}
