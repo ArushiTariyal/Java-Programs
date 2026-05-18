903ceda2c55aa16b2e8275475a62101ec56744a2
Arushi Tariyal <112552758+ArushiTariyal@users.noreply.github.com>
2026-05-18 09:54:19 +0530

    Add files via upload

---
+++ Candy_Greedy_Optimal.java
@@ -0,0 +1,37 @@
+public class Candy_Greedy_Optimal {
+    class Solution {
+        public int candy(int[] ratings) {
+            int sum = 1;
+            int i = 1;
+
+            while(i<ratings.length){
+                if(ratings[i]==ratings[i-1]){
+                    sum += 1;
+                    i++;
+                    continue;
+                }
+
+                int peak = 1;
+                while(i<ratings.length && ratings[i]>ratings[i-1]){
+                    peak++;
+                    sum += peak;
+                    i++;
+                }
+
+                int down = 1;
+
+                while(i<ratings.length && ratings[i]<ratings[i-1]){
+                    sum += down;
+                    down++;
+                    i++;
+                }
+
+                if(down>peak){
+                    sum += (down-peak);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
